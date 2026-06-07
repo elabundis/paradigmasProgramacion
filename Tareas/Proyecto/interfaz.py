3b9c3425a6c8b76e5913403fae7ee1dacd246025
Jesus Abundis <elabundis@gmail.com>
2026-06-06 17:18:41 -0700

    Add interface for application

---
+++ Tareas/Proyecto/interfaz.py
@@ -0,0 +1,21 @@
+# Eres libre de usar tu imaginación aquí
+# Imprime aquí mensajes en pantalla que guien a un usuario a usar tu sistema de
+# recomendaciones.
+
+# En este archivo debes mandar llamar a los dos módulos:
+#    libros y recomendaciones
+# ya que los necesitaras para darle respuestas al usuario.
+
+# Debes darle un mensaje al usuario con respecto a lo que hace tu programa.
+# Debes preguntar si desea un resumen de palabras de un libro o si desea
+# recomendaciones de libros en base a un libro que le haya gustado.
+# Debes mostrar una lista con índices y nombres de los libros.
+
+# Si desea un resumen debes preguntarle de qué libro (que te de el índice
+# correspondiente) y cuantas palabras desea y despues imprimir la información
+# con un formato bonito.
+
+# Si desea recomendaciones de libro: pregúntale que libro le gusta (que te de
+# un índice de acuerdo a la lista que le presentaste) y despues le preguntas
+# cuantos libros quiere que le recomiendes. Posteriormente, le muestras los
+# nombres de los libros recomendados con un buen formato.
